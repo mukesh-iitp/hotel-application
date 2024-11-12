2dfe7391abc00240f7c60b9441ba684f7df1b40b
Mukesh Kumar <ermkit@gmail.com>
2024-11-13 02:19:30 +0530

    add method level security - PreAuthorization

--- src/main/java/com/cn/hotel/controller/HotelController.java
+++ src/main/java/com/cn/hotel/controller/HotelController.java
@@ -3,6 +3,7 @@ package com.cn.hotel.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ public class HotelController {
 	HotelService hotelService;
 	
 	@PostMapping("/create")
+	@PreAuthorize("hasRole('ADMIN')")
 	public void createHotel(@RequestBody HotelRequest hotelRequest)
 	{
 
@@ -30,18 +32,21 @@ public class HotelController {
 	} 
 	
 	@GetMapping("/id/{id}")
+	@PreAuthorize("hasRole('NORMAL')")
 	public Hotel getHotelById(@PathVariable Long id)
 	{
 		return hotelService.getHotelById(id);
 	}
 	
 	@GetMapping("/getAll")
+	@PreAuthorize("hasRole('ADMIN')")
 	public List<Hotel> getAllHotels()
 	{
 		return hotelService.getAllHotels();
 	}
 	
 	@DeleteMapping("/remove/id/{id}")
+	@PreAuthorize("hasRole('ADMIN')")
 	public void deleteHotelById(@PathVariable Long id)
 	{
 		hotelService.deleteHotelById(id);
